a17433e139b366315cca1f70a6b884f3fce74b50
Abendgast <100733158+Abendgast@users.noreply.github.com>
2023-10-23 21:59:37 +0300

    audioChange.js

--- lab4_PRILEPSKY/Table_2/audioChange.js
+++ lab4_PRILEPSKY/Table_2/audioChange.js
@@ -1,10 +1,4 @@
 function changeAudio(music, video){
-    var videoMap = {
-        'groupmates/music/Papercut.mp3': 'groupmates/Video/1.mp4',
-        'groupmates/music/InsideTheFire.mp3': 'groupmates/Video/2.mp4',
-        'groupmates/music/HellOfANight.mp3': 'groupmates/Video/3.mp4',
-        'groupmates/music/Benzin.mp3': 'groupmates/Video/4.mp4'
-    };
-    document.getElementById('myAudio').setAttribute('src', music);
-    document.getElementById('myVideo').children[0].setAttribute('src', videoMap[music]);
+    document.getElementById('myAudio').setAttribute('src', music)
+    document.getElementById('myVideo').setAttribute('src', video)
 }
